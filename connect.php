630556c09d7b2785f1672480328ac8af1928721b
yin-coder <172569856@qq.com>
2015-11-16 00:25:19 +0800

    添加重复验证

--- connect.php
+++ connect.php
@@ -5,5 +5,4 @@ $sqlpassword = 'yin123';
 $con = mysql_connect($sqladdress,$sqlname,$sqlpassword) or die("数据库链接错误".mysql_error());  
 mysql_select_db("MrWho",$con) or die("数据库访问错误".mysql_error());  
 mysql_query("set names utf8");
-mysql_select_db('students'); 
 ?>
