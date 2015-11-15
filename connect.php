8e014053bd9477162175d17b6e5eb92dd8afcade
CWen <172569856@qq.com>
2015-11-16 00:30:19 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 $sqladdress = 'localhost';
-$sqlname = 'root';
-$sqlpassword = 'yin123';
+$sqlname = ;
+$sqlpassword = ;
 $con = mysql_connect($sqladdress,$sqlname,$sqlpassword) or die("数据库链接错误".mysql_error());  
 mysql_select_db("MrWho",$con) or die("数据库访问错误".mysql_error());  
 mysql_query("set names utf8");
